9375d23fc8c8eb8b0c815851a459cac8c447cc4b
HimanshiPrajapat <115346167+HimanshiPrajapat@users.noreply.github.com>
2024-08-14 11:52:25 +0530

    javascript2

--- Training/node.js
+++ Training/node.js
@@ -1,4 +1,5 @@
-console.log("Hello World");
+//------------------------------------------------------day1-----------------------------------------------------------------------------------------------------
+/*console.log("Hello World");
 // alert ("Hello techno")
 
 //difference between var, let ,const (why there are three variable defining keywords var was earlier used)
@@ -101,5 +102,137 @@ console.log(c===e); //it also compares datatypes
 let isloggedin=true; //=20 
 let isAdmin=true;    //=10 
 console.log("Is the user logged in or admin",isloggedin && isAdmin)  //if 0&&num return 0(consider as -ve) if num1&& num2 return num2 
- console.log(10 || 20) //as it only check first value whether it is true or false
+ console.log(10 || 20) //as it only check first value whether it is true or false*/
+//------------------------------------------------------------------Day2----------------------------------------------------------------------------
+
+//conditional statements
+//if else, switch ,ternary operators
+//functions
+//array ,object
+//Loops
+
+// let count=40;
+// if(count==20){
+//     console.log("The count is 20");
+// }else if(count<20){
+//      console.log("The count is less than 20");
+// }
+// else{
+//     console.log("The count is greater than 20");
+// }
+
+// use prompt to take input from user
+//node doesn't take input it will only take in browser console
+// let num=prompt("Enter a number");
+//  let username=prompt("Enter Username:");
+//  let pw=+prompt("Enter Password:");
+//  if(username=="Admin" && pw=="123"){ //or use if(username==="Admin" && pw==="123") without type casting password
+//     console.log("Auth Successfull");
+//  }else
+//  {
+//     console.log("Auth Failed");
+//  }
+
+//ternary operator
+// let count=20;
+// count===20 ? console.log("Count is 20") : console.log("Count is not 20")
+// let count=10;
+// count===20 ? console.log("Count is 20") : count >20 ? console.log("Count is greater than 20"): console.log("Count is less than 20")
+// let username=prompt("Enter Username:");
+// let pw=+prompt("Enter Password:");
+// username=="Admin" && pw=="123" ? console.log("Auth successfull") : console.log("Auth Failed")
+
+let dayNumber = 3;
+let dayName;
+switch (dayNumber) {
+  case 1:
+    dayName = "Monday";
+  break;
+  case 2:
+    dayName = "Tuesday";
+  break;
+  case 3:
+    dayName = "Wednesday";
+  break;
+  case 4:
+    dayName = "Thrusday";
+  break;
+  case 5:
+    dayName = "Friday";
+  break;
+  case 6:
+    dayName = "Saturday";
+  break;
+  case 7:
+    dayName = "Sunday";
+  break;
+  default:
+    dayName="invalid day number";
+}
+console.log(dayName)
+//without break it will execute all cases after the case become true
+
+//Types of defining functions: function declaration ,function expression ,arrow function.
+//function declaration 
+// function sum(){
+//     console.log("calling sum function by function ");
+// }
+// sum();
+//function expression 
+// const sum =function(){
+//     console.log("calling sum function by function expression");
+// }
+// sum();
+ //arrow function 
+let sum =(a,b)=>{
+    console.log("calling sum function by Arrow function "); //most commonly used
+    return(a+b);
+}
+//or if return is single line the use
+//let sum=(a,b)=>a+b
+let add = sum(5,2);
+console.log(add);
+
+//Scopes in js: earlier global/local or functional scope then block scope (rest all curley braces variable has block scope)came
+// var -global scope variable if defined in function act as local never act as block 
+// let & const - block scope variable
+// {
+//     let a =20; //error: a is not defined
+// }
+// console.log(a);
+// {
+//     var a =45;  
+//     console.log(a);   //let treat ar different variables while var doesn't
+// }
+// {
+//     var a="abc" 
+// }
+// console.log(a);  //shows himanshi 
+
+//array and object
+let arr=[10,20,50,70,80]
+console.log(arr[arr.length-1]); //accessing last element
+let nestedarr= [ 10,20,30,[1,2,3,4],[30,40,50],["aa","bb",[true,false]]];
+let newarr=nestedarr[nestedarr.length-1]
+let newarr1=newarr[newarr.length-1]     
+
+console.log(newarr)
+console.log(newarr1[1])  //access the [true,false] last element of nested arr
+// console.log(arr.length)
+// arr[arr.length]="Himanshi"
+// console.log(arr)
+// console.log(arr.length)
+
+//objects
+let obj={
+    "Firstname": "Himanshi",      //can be written as variable or string
+    "lastname": "Prajapat"
+}
+console.log(obj.Firstname)
+obj.lastname="jain"
+console.log(obj.lastname)
+obj.section="SID"
+//obj[SID]="12345" //gives the error SID not defined hence we always use key values in square bracket in String only
+obj["SID"]="12345"
+console.log(obj)
 
