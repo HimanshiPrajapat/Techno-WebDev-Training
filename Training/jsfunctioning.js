9dcff0ab1cbfadc81efc23a9163d94fa27e1f42d
HimanshiPrajapat <115346167+HimanshiPrajapat@users.noreply.github.com>
2024-08-27 23:10:20 +0530

    javascript8

--- Training/jsfunctioning.js
+++ Training/jsfunctioning.js
@@ -179,19 +179,167 @@
 // .catch((e)=>{
 //     console.log(e);
 // })
-document.getElementById("product-button").addEventListener("click",function(){
-    fetch("https://fakestoreapi.com/products")
-    .then((res)=>res.json())
-    .then((data)=>{
-        data.forEach((item) => {
-            console.log(item.image);
-            let newDiv = document.createElement("div");
-            newDiv.innerHTML = `<img style='width : 100px' src=${item.image} alt='Product-Image' ><h3>${item.title}</h3><p>${item.price}</p>`;
-            let myDiv = document.getElementById("products");
-            myDiv.appendChild(newDiv);
-        });
-    })
-    .catch((error)=>{
-        console.log(error);
-    })
-});
+// document.getElementById("product-button").addEventListener("click",function(){
+//     fetch("https://fakestoreapi.com/products")
+//     .then((res)=>res.json())
+//     .then((data)=>{
+//         data.forEach((item) => {
+//             console.log(item.image);
+//             let newDiv = document.createElement("div");
+//             newDiv.innerHTML = `<img style='width : 100px' src=${item.image} alt='Product-Image' ><h3>${item.title}</h3><p>${item.price}</p>`;
+//             let myDiv = document.getElementById("products");
+//             myDiv.appendChild(newDiv);
+//         });
+//     })
+//     .catch((error)=>{
+//         console.log(error);
+//     })
+// });
+
+// ----------------------------------------------------------------Day-8 --------------------------------------------------------------------------------------------
+
+//Async Await    //this below code will work asynchronously
+// async function fetchUserData(){
+//      try{
+//         let response= await fetchData()          //wait till data is not fetched
+//      console.log(response);
+//      }
+//      catch(error){
+//         console.log(error);
+//      }
+// }
+// fetchUserData();
+
+//This -globally print then gives blank object in node and window object in browser  or printed in function gives reference of that function
+// let user ={
+//     username: "Himanshi",
+//     age: "20",
+//     getUserDetails : function(){
+//         console.log(`Hello, My username is ${this.username} and My age is ${this.age}`);
+//         console.log(this)
+//     }
+// }
+// console.log(this)
+// user.getUserDetails();
+
+//constructor - functions which forms an object and works as classes (there are no classes)
+//constructor function  --function first latier is always capital
+//not right way to create constructor
+// function Person(name,age){
+//     const personobj = {
+//           username:name,
+//           age:age,
+//           greet: function(){
+//             console.log(`Hello, My username is ${this.username} and My age is ${this.age}`);  // this is called template string 
+//           }
+//     }
+//     return personobj;
+// }
+// const person1= Person("Himanshi","20")
+// const person2= Person("Karan","40")
+// person1.greet();
+// person2.greet();
+
+//Right way
+// function Person(name,age){
+//     this.name=name;
+//     this.age=age;
+//     // this.greet = function(){
+//     //                 console.log(`Hello, My username is ${this.name} and My age is ${this.age}`);  // this is called template string 
+//     // }
+// }
+// Person.prototype.greet=function(){
+//                     console.log(`Hello, My username is ${this.name} and My age is ${this.age}`);  // this is called template string 
+//     }
+// const person1= new Person("Himanshi","20")
+// const person2= new Person("Karan","40")
+// console.log(person1)
+// console.log(person2)
+// person1.greet();
+// person2.greet();
+
+//classes
+class Person{
+    constructor(name,age){
+             this.name=name;
+             this.age = age;
+}
+greet(){
+    console.log(`Hello, My username is ${this.name} and My age is ${this.age}`)
+}
+}
+const person1= new Person("Jatin","55");
+console.log(person1)
+
+
+//Inheritance concept
+class Animal{
+    constructor(name){
+        this.name=name;
+    }
+    speak(){
+        console.log(`${this.name} makes noise`);
+    }
+}
+class Dog extends Animal{
+    constructor(name){
+        super(Animal)
+        this.name=name;
+    }
+    speak(){
+        console.log(`${this.name} barks`);  //function overridding
+    }
+}
+const Dogobj = new Dog("Dog");
+console.log(Dogobj);
+class Cat extends Animal{
+    
+    constructor(name){
+        super(Animal)
+        this.name=name;
+    }
+    speak(){
+        console.log(`${this.name} meows`);  //function overridding
+    }
+}
+
+const Catobj = new Cat("Cat");
+console.log(Catobj);
+Dogobj.speak();
+Catobj.speak();
+
+// class BankAccount{
+//      balance=0;
+//     constructor(Bankname,Amount){
+//         this.Bankname=Bankname;
+//         this.balance+=Amount;
+//     }
+//     BankStatus(){
+//         console.log(`Your BankAccount is in ${this.Bankname} with a balance of ${this.balance}`)
+//     }
+// }
+// const Bankinfo1=new BankAccount("State Bank of India",4500);
+// console.log(Bankinfo1);
+// Bankinfo1.BankStatus();
+// const Bankinfo2=new BankAccount("ICICI",3000);
+// console.log(Bankinfo2);
+// Bankinfo2.BankStatus();
+
+class BankAccount{
+        #balance = 0;
+    
+        constructor(owner){
+            this.owner = owner;
+        }
+    
+        deposit(amount){
+            this.#balance += amount;
+            console.log(`${this.owner} deposited ${amount} , Current Balance : ${this.#balance}`)
+        }
+    }
+    const Bankinfo1=new BankAccount("Krishna Gupta");
+    console.log(Bankinfo1)
+    Bankinfo1.deposit(10000)
+    const Bankinfo2=new BankAccount("Pavan Jain");
+    console.log(Bankinfo2)
+    Bankinfo1.deposit(1000)
